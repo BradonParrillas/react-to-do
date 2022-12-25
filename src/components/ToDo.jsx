cedb5bbf56c5548c514347e82e57ab9d1ef010a0
Daniel Parrillas <daniel16parrillas@gmail.com>
2022-12-24 21:52:42 -0600

    task saved in localstorage

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -3,13 +3,14 @@ import TaskForm from "./TaskForm";
 import { useState } from "react";
 
 const ToDo = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")));
 
   const addTask = (task) => {
     if (task.text.trim()) {
       task.text = task.text.trim();
       task.state = false;
       const uptatedTasks = [task, ...tasks];
+      localStorage.setItem("tasks", JSON.stringify(uptatedTasks));
       setTasks(uptatedTasks);
       console.log("%ctask added", "color: green");
     }
@@ -17,6 +18,7 @@ const ToDo = () => {
 
   const deleteTask = (id) => {
     const uptatedTasks = tasks.filter((task) => task.id !== id);
+    localStorage.setItem("tasks", JSON.stringify(uptatedTasks));
     setTasks(uptatedTasks);
     console.log("%cdeleted task", "color:orange");
   };
@@ -28,6 +30,7 @@ const ToDo = () => {
       }
       return task;
     });
+    localStorage.setItem("tasks", JSON.stringify(uptatedTasks));
     setTasks(uptatedTasks);
     console.log("%cupdate task", "color:aquamarine");
   };
